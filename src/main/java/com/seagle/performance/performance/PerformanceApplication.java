4c45db162867ab10d9a8bdf258d66f046f9a8892
帅维灿 <shuaiweican@douyu.tv>
2019-03-22 14:59:20 +0800

    发布tomcat配置

--- src/main/java/com/seagle/performance/performance/PerformanceApplication.java
+++ src/main/java/com/seagle/performance/performance/PerformanceApplication.java
@@ -2,16 +2,23 @@ package com.seagle.performance.performance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class PerformanceApplication {
+public class PerformanceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PerformanceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PerformanceApplication.class);
     }
 
     //处理跨域问题
